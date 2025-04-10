ca6a43555a7d855310f1328f74bca7a0458699f2
Madelyn Gross <mgross10@hydra7.eecs.utk.edu>
2025-04-10 13:26:30 -0400

    okay I think we are done

--- makefile
+++ makefile
@@ -1,9 +1,47 @@
+# CC = gcc
+
+# INCLUDES = -I/home/mrjantz/cs360/include
+# LIBDIR = /home/mrjantz/cs360/lib
+
+# CFLAGS = $(INCLUDES)
+# LIBS = $(LIBDIR)/libfdr.a
+
+# EXECUTABLES = bin/jshell
+
+# all: $(EXECUTABLES)
+
+# testers: bin/cattostde \
+#          bin/strays \
+#          bin/strays-files \
+#          bin/strays-fsleep \
+#          bin/strays-sleep \
+
+# bin/jshell: src/jshell.c
+# 	$(CC) $(CFLAGS) -o bin/jshell src/jshell.c $(LIBS)
+
+# bin/strays: src/strays.c
+# 	$(CC) -o bin/strays src/strays.c
+
+# bin/cattostde: src/cattostde.c
+# 	$(CC) -o bin/cattostde src/cattostde.c
+
+# bin/strays-files: src/strays-files.c
+# 	$(CC) -o bin/strays-files src/strays-files.c
+
+# bin/strays-fsleep: src/strays-fsleep.c
+# 	$(CC) -o bin/strays-fsleep src/strays-fsleep.c
+
+# bin/strays-sleep: src/strays-sleep.c
+# 	$(CC) -o bin/strays-sleep src/strays-sleep.c
+
 CC = gcc
 
 INCLUDES = -I/home/mrjantz/cs360/include
 LIBDIR = /home/mrjantz/cs360/lib
 
-CFLAGS = $(INCLUDES)
+# Add -fsanitize=address for AddressSanitizer and -g for debug symbols
+CFLAGS = -g -fsanitize=address $(INCLUDES)
+LDFLAGS = -fsanitize=address
 LIBS = $(LIBDIR)/libfdr.a
 
 EXECUTABLES = bin/jshell
@@ -17,19 +55,19 @@ testers: bin/cattostde \
          bin/strays-sleep \
 
 bin/jshell: src/jshell.c
-	$(CC) $(CFLAGS) -o bin/jshell src/jshell.c $(LIBS)
+	$(CC) $(CFLAGS) -o bin/jshell src/jshell.c $(LIBS) $(LDFLAGS)
 
 bin/strays: src/strays.c
-	$(CC) -o bin/strays src/strays.c
+	$(CC) $(CFLAGS) -o bin/strays src/strays.c $(LDFLAGS)
 
 bin/cattostde: src/cattostde.c
-	$(CC) -o bin/cattostde src/cattostde.c
+	$(CC) $(CFLAGS) -o bin/cattostde src/cattostde.c $(LDFLAGS)
 
 bin/strays-files: src/strays-files.c
-	$(CC) -o bin/strays-files src/strays-files.c
+	$(CC) $(CFLAGS) -o bin/strays-files src/strays-files.c $(LDFLAGS)
 
 bin/strays-fsleep: src/strays-fsleep.c
-	$(CC) -o bin/strays-fsleep src/strays-fsleep.c
+	$(CC) $(CFLAGS) -o bin/strays-fsleep src/strays-fsleep.c $(LDFLAGS)
 
 bin/strays-sleep: src/strays-sleep.c
-	$(CC) -o bin/strays-sleep src/strays-sleep.c
+	$(CC) $(CFLAGS) -o bin/strays-sleep src/strays-sleep.c $(LDFLAGS)
--- src/jshell.c
+++ src/jshell.c
@@ -1,3 +1,5 @@
+//Lab 8: JShell
+//Madelyn Gross
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -10,17 +12,20 @@
 #include "dllist.h"
 #include "jrb.h"
 
+//command struct from TA Maria
+//I got rid of the argcs array because I never used it, idk what I was supposed to do with it
+//but I didnt do it
 typedef struct command{
     char *stdin;          /* Filename from which to redirect stdin.  NULL if empty.*/ 
     char *stdout;         /* Filename to which to redirect stdout.  NULL if empty.*/ 
     int append_stdout;    /* Boolean for appending.*/ 
     int wait;             /* Boolean for whether I should wait.*/ 
     int n_commands;       /* The number of commands that I have to execute*/ 
-    int *argcs;           /* argcs[i] is argc for the i-th command*/ 
     char ***argvs;        /* argcv[i] is the argv array for the i-th command*/ 
     Dllist comlist;       /* I use this to incrementally read the commands.*/ 
-  } Command;
+} Command;
 
+//initializing this command, returning it
 Command* initializeCommand(){
     Command *newCommand = malloc(sizeof(Command));
 
@@ -29,13 +34,13 @@ Command* initializeCommand(){
     newCommand->append_stdout = 0;
     newCommand->wait = 1;
     newCommand->n_commands = 0;
-    newCommand->argcs = NULL;
     newCommand->argvs = NULL;
     newCommand->comlist = new_dllist();
 
     return newCommand;
 }
 
+//just freeing the command
 void freeCommand(Command *commandToFree){
     if(commandToFree->stdin != NULL){
         free(commandToFree->stdin);
@@ -43,6 +48,7 @@ void freeCommand(Command *commandToFree){
     if(commandToFree->stdout != NULL){
         free(commandToFree->stdout);
     }
+    //I am going through the comlist and freeing the arrays, each argument will be freed
     if(commandToFree->comlist != NULL){
         Dllist temp;
 
@@ -55,75 +61,31 @@ void freeCommand(Command *commandToFree){
         }
         free_dllist(commandToFree->comlist);
     }
-    if(commandToFree->argcs != NULL){
-        free(commandToFree->argcs);
-    }
+    //because this is simply a pointer to the arrays in comlist I have already freed everything inside
+    //each argument of this array so I am just freeing this list
     if(commandToFree->argvs != NULL){
         free(commandToFree->argvs);
     }
-    
+    //freeing the command itself
+    free(commandToFree);
 }
 
-void print_comlist(Command *cmd) {
-    Dllist node;
-    int cmd_num = 0;
-
-    printf("=== Printing comlist ===\n");
-
-    dll_traverse(node, cmd->comlist) {
-        char **argv = (char **) jval_v(node->val);
-        printf("comlist Command %d: ", cmd_num);
-
-        for (int i = 0; argv[i] != NULL; i++) {
-            printf("'%s' ", argv[i]);
-        }
-
-        printf("\n");
-        cmd_num++;
-    }
-
-    printf("Total commands in comlist: %d\n", cmd->n_commands);
-
-    // Print argvs and argcs if available
-    if (cmd->argvs != NULL && cmd->argcs != NULL) {
-        printf("=== Printing argvs and argcs ===\n");
-        for (int i = 0; i < cmd->n_commands; i++) {
-            printf("argvs[%d] (argc: %d): ", i, cmd->argcs[i]);
-            for (int j = 0; j < cmd->argcs[i]; j++) {
-                printf("'%s' ", cmd->argvs[i][j]);
-            }
-            printf("\n");
-        }
-    } else {
-        printf("argvs and argcs not set yet.\n");
-    }
-}
-
-
-int getArgc(char **argv){
-    int count = 0;
-    while(argv[count] != NULL){
-        count++;
-    }
-    return count;
-}
-
+//this function is just assigning the argvs from comlist to the array of argcs
 void getArgsFromComList(Command *thisCommand){
-    thisCommand->argcs = malloc(sizeof(int) * thisCommand->n_commands);
     thisCommand->argvs = malloc(sizeof(char **) * thisCommand->n_commands);
 
     Dllist temp;
-
     int i = 0;
     dll_traverse(temp, thisCommand->comlist){
         char **argv = (char **) jval_v(temp->val);
-        thisCommand->argcs[i] = getArgc(argv);
         thisCommand->argvs[i] = argv;
         i++;
     }
 }
 
+//this is my main execution function
 void executeCommand(Command *commandToExecute){
+    //initializing the variables I will need
     pid_t pid;
     int fileDescriptorOpen;
     int fileDescriptorEnd = 0;
@@ -131,11 +93,14 @@ void executeCommand(Command *commandToExecute){
     JRB pids = make_jrb();
     JRB tmp;
 
+    //for each command I have in my commandToExecute struct
     for(int j = 0; j < commandToExecute->n_commands; j++){
+        //flushing stdin, stdout, and stderr because they said to 
         fflush(stdin);
         fflush(stdout);
         fflush(stderr);
 
+        //creating pipe if I have more than 1 command
         if(j < commandToExecute->n_commands - 1){
             if(pipe(ends) < 0){
                 fprintf(stderr, "Error creating pipe\n");
@@ -143,53 +108,65 @@ void executeCommand(Command *commandToExecute){
             }
         }
 
+        //forking 
         pid = fork();
 
-
+        //if I am in a child
         if(pid == 0){
+            //if I am in my first command and stdin has something to read from
             if(j == 0 && commandToExecute->stdin != NULL){
                 fileDescriptorOpen = open(commandToExecute->stdin, O_RDONLY);
-                if(fileDescriptorOpen == -1){
+                if(fileDescriptorOpen < 0){
                     fprintf(stderr, "Error opening file %s\n", commandToExecute->stdin);
                     exit(1);
                 }
                 dup2(fileDescriptorOpen, STDIN_FILENO);
                 close(fileDescriptorOpen);
             }
+            //if stdout has anything 
             if(commandToExecute->stdout != NULL){
+                //opening to append or truncate depending on the input
                 if(commandToExecute->append_stdout == 1){
                     fileDescriptorOpen = open(commandToExecute->stdout, O_WRONLY | O_CREAT | O_APPEND, 0644);
                 }else{
                     fileDescriptorOpen = open(commandToExecute->stdout, O_WRONLY | O_CREAT | O_TRUNC, 0644);
                 }
-                if(fileDescriptorOpen == -1){
+                if(fileDescriptorOpen < 0){
                     fprintf(stderr, "Error opening file %s\n", commandToExecute->stdout);
                     exit(1);
                 }  
                 dup2(fileDescriptorOpen, STDOUT_FILENO);
                 close(fileDescriptorOpen);
             }
+            //if this is not the first command
             if(j > 0){
                 dup2(fileDescriptorEnd, STDIN_FILENO);
-                //close(fileDescriptorEnd);
+                close(fileDescriptorEnd);
             }
+            //if this is not the last command
             if(j < commandToExecute->n_commands - 1){
                 close(ends[0]);
                 dup2(ends[1], STDOUT_FILENO);
                 close(ends[1]);
             }
+            //execute the command for this iteration and error to not fork bomb
+            //because fork bombing is scary and awful and horrible and plank says you shouldnt do it
             execvp(commandToExecute->argvs[j][0], commandToExecute->argvs[j]);
             fprintf( stderr, "%s: No such file or directory\n", commandToExecute->argvs[j][0]);
             exit(1); 
+        //if I am in the partent
         }else{
+            //if we want to wait add them to a tree
             if(commandToExecute->wait == 1){
                 jrb_insert_int(pids, pid, new_jval_i(0));
             }
+            //if this is not the first command I have to close the file descriptor maybe?
             if(commandToExecute->n_commands > 1){
                 if(fileDescriptorEnd != 0){
                     close(fileDescriptorEnd);
                 }
             }
+            //if this is not the last command close the last end and reset the file descriptor
             if(j < commandToExecute->n_commands - 1){
                 close(ends[1]);
                 fileDescriptorEnd = ends[0];
@@ -197,6 +174,8 @@ void executeCommand(Command *commandToExecute){
         }
     }
 
+    //this is where we wait for the children to finish making sure to delete them to the tree when
+    //the do so that we can finish
     while(!jrb_empty(pids)){
         pid = wait(NULL);
         tmp = jrb_find_int(pids, pid);
@@ -204,48 +183,43 @@ void executeCommand(Command *commandToExecute){
             jrb_delete_node(tmp);
         }
     }
+
+    jrb_free_tree(pids);
 }
 
-
+//main
 int main(int argc, char const *argv[])
 {
+    //new input struct to read the files
     IS is;
-
     is = new_inputstruct(NULL);
 
     if(is == NULL) {
         fprintf(stderr, "file cannot be opened");
         return 1;
     }
+
     Command *thisCommand = initializeCommand();
+
+    //just reading all of the input and stuff
     while(get_line(is) >= 0){
-        //printf("First field: '%s' (NF: %d)\n", is->fields[0], is->NF);
         if(is->NF > 0 && strcmp(is->fields[0], "#") != 0){
-            // printf("passing the first if statement\n");
             if(strcmp(is->fields[0], "<") == 0){
-                // printf("this line contains a <\n");
                 thisCommand->stdin = strdup(is->fields[1]);
             }else if(strcmp(is->fields[0], ">") == 0){
-                // printf("this line contains a >\n");
                 thisCommand->stdout = strdup(is->fields[1]);
             }else if(strcmp(is->fields[0], ">>") == 0){
-                // printf("this line contains a >>\n");
                 thisCommand->append_stdout = 1;
                 thisCommand->stdout = strdup(is->fields[1]);
             }else if(strcmp(is->fields[0], "NOWAIT") == 0){
-                // printf("this line contains a NOWAIT\n");
                 thisCommand->wait = 0;
             }else if(strcmp(is->fields[0], "END") == 0){
-                // printf("this line contains a END\n");
-                // printf("process argument\n");
+                //this is where everything gets called because we have come across an end line
                 getArgsFromComList(thisCommand);
-                // print_comlist(thisCommand);
                 executeCommand(thisCommand);
                 freeCommand(thisCommand);
                 thisCommand = initializeCommand();
             }else {
-                // printf("this line contains a different argument\n");
-                // printf("line 1?: %s\n", is->text1);
                 thisCommand->n_commands++;
                 char **args = malloc(sizeof(char*) * (is->NF + 1));
                 for(int i = 0; i < is->NF; i++){
@@ -254,8 +228,10 @@ int main(int argc, char const *argv[])
                 args[is->NF] = NULL;
                 dll_append(thisCommand->comlist, new_jval_v(args));
             }
-            // printf("-------------------\n");
         }
     }
+    //freeing the last command that I initialize after I free the last real one
+    freeCommand(thisCommand);
+    jettison_inputstruct(is);
     return 0;
 }
